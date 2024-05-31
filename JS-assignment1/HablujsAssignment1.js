531ca3cf56c63feb4e0e59604a52a170f4f0caab
SajiaSohana <164738600+SajiaSohana@users.noreply.github.com>
2024-05-31 16:15:56 +0600

    Update HablujsAssignment1.js

--- JS-assignment1/HablujsAssignment1.js
+++ JS-assignment1/HablujsAssignment1.js
@@ -3,9 +3,6 @@ let carName = "Volvo";
 console.log("carName:", carName);
 
 // 2. On one single line, declare three variables with the following names and values:
-// firstName = "John"
-// lastName = "Doe"
-// age = 35
 //let firstName = "John", lastName = "Doe", age = 35;
 //console.log("firstName:", firstName);
 //console.log("lastName:", lastName);
